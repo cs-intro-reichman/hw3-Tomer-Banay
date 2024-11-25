a66c8ba91e3d161ddd7bbbce3f0bebbfda7c8701
Tomer-Banay <tomer.banay@post.runi.ac.il>
2024-11-25 15:38:51 +0200

    3/3

--- Anagram.java
+++ Anagram.java
@@ -57,7 +57,7 @@ public class Anagram {
 		String newString =  "";
 		for (int i = 0 ; i < str.length(); i++) {
 			char ch = str.charAt(i);
-			if ((ch >='a' && ch <= 'z') || (ch >='A' && ch <= 'Z')) {
+			if ((ch >='a' && ch <= 'z') || (ch >='A' && ch <= 'Z') || ch == ' ') {
 				newString += ch; 
 			}
 		}
